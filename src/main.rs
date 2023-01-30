a984292f08eaa1b8d9122c275a568344927a4da4
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-01-31 02:59:24 +0900

    Use lalrpop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-extern crate combine;
-use combine::parser::char::{letter, space};
-use combine::{many1, sep_by, Parser};
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub calculator1);
 
 #[test]
-fn first_sample() {
-    let word = many1(letter());
-
-    let mut parser = sep_by(word, space()).map(|mut words: Vec<String>| words.pop());
-
-    let result = parser.parse("Pick up that word!");
-    assert_eq!(result, Ok((Some("word".to_string()), "!")));
+fn calculator1() {
+    assert!(calculator1::TermParser::new().parse("22").is_ok());
+    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+    assert!(calculator1::TermParser::new().parse("(((22)))").is_ok());
+    assert!(calculator1::TermParser::new().parse("((22))").is_ok());
 }
-
 #[cfg(not(test))]
 fn main() {
     println!("Hello, world!");
