b2de17d605af4fff446dfcd8086795bf895c207e
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-02-01 02:08:44 +0900

    Add calculator4

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ extern crate lalrpop_util;
 lalrpop_mod!(pub calculator1);
 lalrpop_mod!(pub calculator2);
 lalrpop_mod!(pub calculator3);
+lalrpop_mod!(pub calculator4);
+
+pub mod ast;
 
 #[test]
 fn calculator1() {
@@ -26,6 +29,12 @@ fn calculator3() {
     assert_eq!(calculator3::ExprParser::new().parse("2+3"), Ok(5));
     assert_eq!(calculator3::ExprParser::new().parse("2+3*4"), Ok(14));
     assert_eq!(calculator3::ExprParser::new().parse("2*3+4"), Ok(10));
+}
+
+#[test]
+fn calculator4() {
+    let expr = calculator4::ExprParser::new().parse("22*44+66").unwrap();
+    assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
 }
 
 #[cfg(not(test))]
