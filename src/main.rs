34844e46decdd241559214b3dffc893637e1e636
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-02-01 01:35:35 +0900

    Add calculator3

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate lalrpop_util;
 
 lalrpop_mod!(pub calculator1);
 lalrpop_mod!(pub calculator2);
+lalrpop_mod!(pub calculator3);
 
 #[test]
 fn calculator1() {
@@ -19,6 +20,14 @@ fn calculator2() {
     assert!(calculator2::TermParser::new().parse("(((22)))").is_ok());
     assert!(calculator2::TermParser::new().parse("((22))").is_ok());
 }
+
+#[test]
+fn calculator3() {
+    assert_eq!(calculator3::ExprParser::new().parse("2+3"), Ok(5));
+    assert_eq!(calculator3::ExprParser::new().parse("2+3*4"), Ok(14));
+    assert_eq!(calculator3::ExprParser::new().parse("2*3+4"), Ok(10));
+}
+
 #[cfg(not(test))]
 fn main() {
     println!("Hello, world!");
