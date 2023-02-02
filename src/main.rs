1b72a8429620a3cf25cbea4ad52effaa4fc828bb
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-02-03 03:40:56 +0900

    Add definitions of basic tokens to the parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub parser);
+
+pub mod ast;
+
+#[test]
+fn parser_test() {
+    assert_eq!(
+        parser::ExprsParser::new().parse("(").unwrap(),
+        ast::Expr::Empty
+    )
+}
+
 fn main() {
     println!("Hello, world!");
 }
